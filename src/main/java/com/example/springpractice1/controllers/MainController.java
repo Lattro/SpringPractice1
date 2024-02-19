43dca22a828e41a729e2c2b9c1624047a157e9d0
Lattro <artm.plyuto.98@mail.ru>
2024-02-19 21:02:16 +0300

    SpringHomeTask1

---
+++ src/main/java/com/example/springpractice1/controllers/MainController.java
@@ -0,0 +1,23 @@
+package com.example.springpractice1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/aboutus")
+public class MainController
+{
+    @GetMapping
+    public String getMainPage()
+    {return "about_us";}
+}
+//
+//@Controller
+//@RequestMapping("/")
+//public class MainController
+//{
+//    @GetMapping
+//    public String getMainPage()
+//    {return "index";}
+//}
